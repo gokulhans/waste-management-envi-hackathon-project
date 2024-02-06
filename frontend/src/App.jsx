ce8685fdb1e07ddad1411a59cf74cdec46611539
Gokul <gokulhansv@gmail.com>
2024-02-06 23:06:12 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,107 @@
+import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar/Navbar";
-
+import ForgotPassword from "./pages/Shared/ForgotPassword/ForgotPassword";
+import SignIn from "./pages/Shared/SignIn/SignIn";
+import SignUp from "./pages/Shared/SignUp/SignUp";
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import UserHome from "./pages/User/UserHome/UserHome";
+import Subscription from "./pages/User/Subscription/Subscription";
+import BasketLocation from "./pages/User/BasketLocation/BasketLocation";
+import SingleBasket from "./pages/User/SingleBasket/SingleBasket";
+import RequestBasket from "./pages/User/RequestBasket/RequestBasket";
+import BookWMS from "./pages/User/BookWMS/BookWMS";
+import EventList from "./pages/User/EventList/EventList";
+import MyProfile from "./pages/User/MyProfile/MyProfile";
 
 function App() {
+  // const [isUser, setIsUser] = useState(localStorage.getItem("isUser"));
+  const [isUser, setIsUser] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(localStorage.getItem("isAdmin"));
   return (
     <>
-      <Navbar/>
+      <Toaster position="top-right" reverseOrder={false} />
+      <div className="bg-blue-100 dark:bg-gray-900 dark:text-gray-50">
+        {/* {
+        
+        isAdmin ? (
+          <>
+            <AdminNavbar isAdmin={isAdmin} setIsAdmin={setIsAdmin} />
+          </>
+        ) : ( */}
+        <>
+          <Navbar isUser={isUser} setIsUser={setIsUser} />
+        </>
+        {/* )} */}
+        <div className="pt-16 min-h-screen flex flex-col items-center justify-center">
+          <Routes>
+            {!isUser ? (
+              <>
+                <Route
+                  path="/"
+                  element={
+                    isAdmin ? (
+                      <Navigate to={"/admin"} />
+                    ) : (
+                      <Navigate to={"/signup"} />
+                    )
+                  }
+                />
+                <Route
+                  path="/signin"
+                  element={<SignIn setIsUser={setIsUser} />}
+                />
+                <Route
+                  path="/signup"
+                  element={<SignUp setIsUser={setIsUser} />}
+                />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
+              </>
+            ) : (
+              <>
+                <Route exact path="/" element={<UserHome isUser={isUser} />} />
+                <Route
+                  exact
+                  path="/subscribe"
+                  element={<Subscription isUser={isUser} />}
+                />
+                <Route
+                  exact
+                  path="/baskets"
+                  element={<BasketLocation isUser={isUser} />}
+                />
+                <Route
+                  exact
+                  path="/basket/:id"
+                  element={<SingleBasket isUser={isUser} />}
+                />
+                <Route
+                  exact
+                  path="/request-basket"
+                  element={<RequestBasket isUser={isUser} />}
+                />
+                <Route
+                  exact
+                  path="/book-wm-service"
+                  element={<BookWMS isUser={isUser} />}
+                />
+                <Route
+                  exact
+                  path="/events"
+                  element={<EventList isUser={isUser} />}
+                />
+                <Route
+                  exact
+                  path="/profile"
+                  element={<MyProfile isUser={isUser} />}
+                />
+                <Route path="*" element={<Error isUser={isUser} />} />
+              </>
+            )}
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
